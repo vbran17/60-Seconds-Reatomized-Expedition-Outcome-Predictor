b32f574748a0cf4d2bfc440292d3f2d9e7810942
lgsoohoo <LucasDevCreations@gmail.com>
2025-03-02 21:21:19 -0500

    Add WinForms Template

---
+++ 60-Seconds-Reatomized-Expedition-Outcome-Predictor/Form1.cs
@@ -0,0 +1,10 @@
+namespace _60_Seconds_Reatomized_Expedition_Outcome_Predictor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 60-Seconds-Reatomized-Expedition-Outcome-Predictor/Program.cs
@@ -0,0 +1,17 @@
+namespace _60_Seconds_Reatomized_Expedition_Outcome_Predictor
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
